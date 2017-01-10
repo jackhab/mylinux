2388682fd28214eb20147fe27aad1e304c24d1e8
jackhab <jackhab@gmail.com>
2017-01-10 12:47:51 +0200

    added in note-list.sh file can be edited

--- notes/note-list.sh
+++ notes/note-list.sh
@@ -3,12 +3,13 @@
 #generate an array of sorted file names and full paths
 IFS=$'\n' Files=($(find $HOME/my/notes -type f -printf '%f\t%p\n' | sort | tr '\t' '\n'))
 
-# for f in "${Files[@]}" ; do echo $f ; done ; exit
-
+#show file selector
 File=$(zenity --title 'Notes' \
                 --list --hide-header --column '' --column ''    \
                 --hide-column 2 --print-column 2    \
                 --width=700 --height=900 \
                 "${Files[@]}")
 
-zenity --text-info --filename=$File --font=Monospace --width=700 --height=900 
+#show selected file, if Cancel pressed is viewer open file in editor
+zenity --text-info --filename=$File --cancel-label="Edit" --font=Monospace --width=700 --height=900 \
+        || xdg-open "$File"

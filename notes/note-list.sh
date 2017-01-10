a26a171b2e801064c9370446fc4271dd1559e54d
jackhab <jackhab@gmail.com>
2017-01-10 18:14:29 +0200

    cosmetic

--- notes/note-list.sh
+++ notes/note-list.sh
@@ -4,12 +4,16 @@
 IFS=$'\n' Files=($(find $HOME/my/notes -type f -printf '%f\t%p\n' | sort | tr '\t' '\n'))
 
 #show file selector
-File=$(zenity --title 'Notes' \
+File=$(zenity   --title 'Notes' --text="" \
                 --list --hide-header --column '' --column ''    \
                 --hide-column 2 --print-column 2    \
                 --width=700 --height=900 \
                 "${Files[@]}")
 
+[[ -z $File ]] && exit 0
+
 #show selected file, if Cancel pressed is viewer open file in editor
-zenity --text-info --filename=$File --cancel-label="Edit" --font=Monospace --width=700 --height=900 \
-        || xdg-open "$File"
+zenity  --text-info --filename=$File --text="" \
+        --cancel-label="Close" --ok-label="Edit" \
+        --font=Monospace --width=700 --height=900 \
+        && xdg-open "$File"

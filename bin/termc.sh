61da3b7898fbc6595e4810905bb5cef297105b01
jackhab <jackhab@gmail.com>
2017-01-16 09:20:24 +0200

    termc graceful exit

--- bin/termc.sh
+++ bin/termc.sh
@@ -8,19 +8,11 @@ cdmc() {
 }
 export -f cdmc
 
-#kill previous sessions to prevent runnning too many clients by accident
-tmux kill-session -t $Session 2>&1 > /dev/null
-
-#setting PROMPT_COMMAND via proxy variable simplifies expansion and escaping
-#PC='cd $(readlink /proc/$(cat $DirFile)/cwd)' ; [[ "$(cat $PrevDir)" != "$PWD" ]] && ll --color=always | sed 1d'
-#		send-keys -t:.1 PROMPT_COMMAND=\'"$PC"\' Enter clear Enter \;\
-
-
 #launch MC, get the PID of pane 0 process and save it to file
 tmux 	new-session "/usr/bin/mc" \;\
 		rename $Session \;\
 		split-window "tmux list-panes -F '#{pane_active} #{pane_pid}' -t $Session | head -n 1 | grep -oP '(?<=\s)\d+' > $DirFile ; bash" \;\
-		bind-key -n F10 kill-session -t $Session \;\
+		bind-key -n F10 select-pane -t:.0 \\\; send-keys F10 \\\; kill-session -t $Session \;\
 		bind-key -n C-Down select-pane -t:.1 \\\; send-keys cdmc Enter \;\
 		bind-key -n C-Up select-pane -t:.0 \\\; send-keys C-r \;\
 		set-option -g status off \;\

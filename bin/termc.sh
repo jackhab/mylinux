3548a554e7ce2f8296ad0581424467fb2c9dafb4
jackhab <jackhab@gmail.com>
2017-01-17 17:09:13 +0200

    cosmetic

--- bin/termc.sh
+++ bin/termc.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 export Session="TERMC-$RANDOM"
-export DirFile="/tmp/$Session"
-export PrevDirFile="/tmp/$Session-prev"
+export McPidFile="/tmp/$Session"
+
 cdmc() {
-	cd $(readlink /proc/$(cat $DirFile)/cwd)
+	cd $(readlink /proc/$(cat $McPidFile)/cwd)
 }
 export -f cdmc
 
 #launch MC, get the PID of pane 0 process and save it to file
 tmux 	new-session "/usr/bin/mc" \;\
 		rename $Session \;\
-		split-window "tmux list-panes -F '#{pane_active} #{pane_pid}' -t $Session | head -n 1 | grep -oP '(?<=\s)\d+' > $DirFile ; bash" \;\
+		split-window "tmux list-panes -F '#{pane_active} #{pane_pid}' -t $Session | head -n 1 | grep -oP '(?<=\s)\d+' > $McPidFile ; bash" \;\
 		bind-key -n F10 select-pane -t:.0 \\\; send-keys F10 \\\; kill-session -t $Session \;\
 		bind-key -n C-Down select-pane -t:.1 \\\; send-keys cdmc Enter \;\
 		bind-key -n C-Up select-pane -t:.0 \\\; send-keys C-r \;\

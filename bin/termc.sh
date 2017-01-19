522e2207a094539dc333a9d6ad5b93805af197be
jackhab <jackhab@gmail.com>
2017-01-19 11:03:08 +0200

    fixed termc F10 exit

--- bin/termc.sh
+++ bin/termc.sh
@@ -12,7 +12,7 @@ export -f cdmc
 tmux 	new-session "/usr/bin/mc" \;\
 		rename $Session \;\
 		split-window "tmux list-panes -F '#{pane_active} #{pane_pid}' -t $Session | head -n 1 | grep -oP '(?<=\s)\d+' > $McPidFile ; bash" \;\
-		bind-key -n F10 select-pane -t:.0 \\\; send-keys F10 \\\; kill-session -t $Session \;\
+        bind-key -n F10 select-pane -t:.0 \\\; send-keys F10 \\\; select-pane -t:.1 \\\; send-keys C-d \\\; \;\
 		bind-key -n C-Down select-pane -t:.1 \\\; send-keys cdmc Enter \;\
 		bind-key -n C-Up select-pane -t:.0 \\\; send-keys C-r \;\
 		set-option -g status off \;\

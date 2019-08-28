f4611f8e2986d9e9402ab30d78d0bc1ef7e78d10
jackhab <jackhab@gmail.com>
2019-08-28 21:55:14 +0300

    nuc done

--- conf/nuc/autostart.sh
+++ conf/nuc/autostart.sh
@@ -1,7 +1,2 @@
-ir-keytable -c -p NEC,RC-5,RC-6,JVC,SONY,LIRC,OTHER -w /etc/rc_keymaps/sonyremote
-
-
-alias ll='ls -l'
-alias al='ll -a'
-
+sudo ir-keytable -c -p NEC,RC-5,RC-6,JVC,SONY,LIRC,OTHER -w /etc/rc_keymaps/sonyremote
 
